95d69394f11dca758acd06306ad8d75bc3965e79
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-08-11 10:59:21 +0530

    chore: track learning module directories

---
+++ 01-javascript-for-react/01-variables-and-data-types/practice.js
@@ -0,0 +1,21 @@
+const studentName = "Rahul";
+let age = 20;
+const isLearningReact = true;
+let company;
+const selectedProject = null;
+
+console.log(studentName);
+console.log(age);
+console.log(isLearningReact);
+console.log(company);
+console.log(selectedProject);
+
+console.log(typeof studentName);
+console.log(typeof age);
+console.log(typeof isLearningReact);
+console.log(typeof company);
+console.log(typeof selectedProject);
+
+age = 21;
+
+console.log(age);
